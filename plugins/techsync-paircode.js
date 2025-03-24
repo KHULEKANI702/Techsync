0ab8cd3dcb9d6134f620fa10450f12954865cc57
𝐌𝐕𝐄𝐋𝐀𝐒𝐄-𝐎𝐅𝐂 <mvelaseofc@gmail.com>
2025-03-25 00:48:38 +0200

    Update techsync-paircode.js

--- plugins/techsync-paircode.js
+++ plugins/techsync-paircode.js
@@ -25,7 +25,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply }) => {
 
         // Fetch pairing code
         //const fetch = require("node-fetch");
-        const response = await fetch(`https://techsync-pair.onrender.com/pair?phone=${q}`);
+        const response = await fetch(`https://techsync-pair8.onrender.com/pair?phone=${q}`);
         const pair = await response.json();
 
         // Check for errors in response

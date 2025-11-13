5a8a8f1720e495bcb65a1e27461c0b0b5aac2497
𝐊𝐇𝐔𝐋𝐄𝐊𝐀𝐍𝐈 𝐌𝐕𝐄𝐋𝐀𝐒𝐄 <n.mvelaseproline@gmail.com>
2025-11-13 16:58:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "JK~H4sIAAAAAAAAAwMAAAAAAAAAAAA=",
+SESSION_ID: process.env.SESSION_ID || "IK~2RkxVCIL#7dvu-lpqqxrvK93zLzGK6uoIIJuaAghGa9IbOJnxDmM",
  
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 
